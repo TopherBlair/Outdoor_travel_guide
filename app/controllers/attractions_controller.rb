db4610b53be41d243e9c4dc52cd2effc27115820
Nick <moft4studio@gmail.com>
2015-01-13 15:43:47 -0700

    generated model and controller

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,50 @@
+class AttractionsController < ApplicationController
+  before_action :set_attraction, only: [:show, :edit, :update, :destroy]
+  
+  def show
+  end
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to @attraction
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @attraction.update_attributes(attraction_params)
+      redirect_to @attraction
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @attraction.destroy
+    redirect_to attractions_url
+  end
+
+  private
+
+  def set_attraction
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:title, :category, :url)
+  end
+
+end
