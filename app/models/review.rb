629ff3995af515d2975c7cbc78f3835c31b8c5c4
Nick <moft4studio@gmail.com>
2015-02-04 14:27:11 -0700

    fixed some css

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,7 @@
 class Review < ActiveRecord::Base
    belongs_to :user
-   has_attached_file :photo, styles: { thumbnail: "50x50>" },
-	   :url	=> "/assets/reviews/:id/:style/:basename.:extenstion",
-	   :path => ":rails_root/public/assets/reviews/:id/:style/:basename.:extention"
-    validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+   has_attached_file :photo, styles: { thumbnail: "50x50>" }
+   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
 
 
