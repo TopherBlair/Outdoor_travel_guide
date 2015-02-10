67eae000cba027a377f7dbea4885dc872c83e31d
Topher Blair <bisonproperties@mac.com>
2015-02-10 08:29:01 -0700

    heroku config

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,7 @@
-Pusher.trails_api_key = ENV["TRAILS_API_KEY"]
-Pusher.google_api_key     = ENV["PUSHER_GOOGLE_API_KEY"]
+Trails.api_key = ENV["TRAILS_API_KEY"]
+Google.key     = ENV["GOOGLE_API_KEY"]
 
-Pusher.IG_CLIENT_ID   = ENV["IG_CLIENT_ID"] 
-Pusher.IG_SECRET_ID:  = ENV["IG_SECRET_ID"]
+IG.CLIENT_ID   = ENV["IG_CLIENT_ID"] 
+IG.SECRET_ID:  = ENV["IG_SECRET_ID"]
 
-Pusher.IG_ACCESS_TOKEN = ENV["IG_ACCESS_TOKEN"]
+IG.ACCESS_TOKEN = ENV["IG_ACCESS_TOKEN"]
