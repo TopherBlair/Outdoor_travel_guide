0fb88a7cb1e5dbab5290cf886245c81eb5aaf8c3
Topher Blair <bisonproperties@mac.com>
2015-02-10 09:48:26 -0700

    heroku formatting

--- config/initializers/pusher.rb
+++
@@ -1,7 +0,0 @@
-Trails.api_key = ENV["TRAILS_API_KEY"]
-Google.api_key     = ENV["GOOGLE_API_KEY"]
-
-IG.CLIENT_ID   = ENV["IG_CLIENT_ID"] 
-IG.SECRET_ID:  = ENV["IG_SECRET_ID"]
-
-IG.ACCESS_TOKEN = ENV["IG_ACCESS_TOKEN"]
